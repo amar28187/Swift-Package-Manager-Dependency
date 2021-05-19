fa79e4f403b76d6118e5fa0fbabf1e0590629142
Amar Makana <amar.makana1@t-mobile.com>
2021-05-19 11:21:36 -0700

    XCode project added

---
+++ SPMDemoApp/SPMDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPMDemoApp
+//
+//  Created by Amar Makana on 5/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
